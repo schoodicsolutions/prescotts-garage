032bc79acebbfa36ca0f7692840736a5480d45d0
Ryan Prescott <rppgoat@gmail.com>
2023-02-24 22:56:13 -0500

    Add testimonials. TODO: fix header

---
+++ src/lib/Review.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+    export let rating: number = 0;
+    export let author: string = '';
+
+    $: ratingToFixed = rating.toFixed(1);
+    $: solidStars = '★'.repeat(Math.min(5, rating));
+    $: emptyStars = '✩'.repeat(5 - rating);
+</script>
+
+<div class="flex-1 flex flex-col justify-between h-full max-w-sm">
+
+    <div class="text-center flex items-center justify-center">
+        <span class="text-yellow-200 text-2xl text-center">{ solidStars }</span>
+        <span class="text-gray-700 text-2xl text-center">{ emptyStars }</span>
+        <span class="text-sm">&nbsp;( { ratingToFixed } )</span>
+    </div>
+
+    <div class="flex gap-2 mb-4">
+        <div class="flex flex-col text-brand justify-center items-center">
+            <div class="w-0.5 bg-[currentColor] grow" />
+            <span class="text-4xl -mt-5 mb-1">&bdquo;</span>
+            <div class="w-0.5 bg-[currentColor] grow" />
+        </div>
+        <blockquote class="text-justify text-md md:text-lg">
+            <slot />
+        </blockquote>
+    </div>
+
+
+    <cite class="w-full block text-right font-medium text-lg md:text-xl">&mdash; { author }</cite>
+
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import Review from "$lib/Review.svelte";
   import Section from "$lib/Section.svelte";
   import VideoBackground from "$lib/VideoBackground.svelte";
   import MediaQuery from "svelte-media-queries";
@@ -44,39 +45,47 @@
                     expertise to get you back on the road safely and efficiently. 
                 </p>
 
-                <div class="w-full flex gap-4">
-                    <a class="button button-brand grow"  use:scrollTo={{section: "contact"}} href="/">Book Now</a>
-                </div>
+                <a class="button button-brand w-full"  use:scrollTo={{section: "contact"}} href="/">Book Now</a>
             </Section>
             <Section scrollRef="services" heading="Services" image="/img/jeep.webp">
                 <p class="text-justify">
-                    Harold Prescott started Prescott's Garage in 2005 with one mission:
-                    to provide reliable, honest auto repair services to the Greater Machias area.
-                    Since then, our shop has become a trusted name in the community, 
-                    thanks to Harold's dedication to his customers and his passion for his work. 
+                    Prescott's Garage offers a range of services to help
+                    keep your vehicle running smoothly and efficiently.
+                    Here are some of the services we offer:
                 </p>
-                <p class="text-justify">
-                    We take pride in providing top-quality auto services at affordable prices.
-                    From routine maintenance to complex repairs, our skilled mechanics have the
-                    expertise to get you back on the road safely and efficiently. 
-                    Come visit us at 67 Main St East Machias, ME 04630, 
-                    and experience the difference that sets us apart.
-                </p>
+                <div class="flex">
+                    <ul class="w-full text-center font-medium text-lg">
+                        <li>Oil Changes</li>
+                        <li>State Inspections</li>
+                        <li>Brake Service</li>
+                        <li>Tire Rotation & Balance</li>
+                        <li>Tire Repair & Replacement</li>
+                    </ul>
+                    <ul class="w-full text-center font-medium text-lg">
+                        <li>Electrical System Repair</li>
+                        <li>Suspension & Steering</li>
+                        <li>Air Conditioning Service</li>
+                        <li>Exhaust System Repair</li>
+                        <li>Custom & Performance</li>
+                    </ul>
+                </div>
             </Section>
-            <Section scrollRef="testimonials" heading="Testimonials" image="/img/wagons.jpg">
-                <p class="text-justify">
-                    Harold Prescott started Prescott's Garage in 2005 with one mission:
-                    to provide reliable, honest auto repair services to the Greater Machias area.
-                    Since then, our shop has become a trusted name in the community, 
-                    thanks to Harold's dedication to his customers and his passion for his work. 
-                </p>
-                <p class="text-justify">
-                    We take pride in providing top-quality auto services at affordable prices.
-                    From routine maintenance to complex repairs, our skilled mechanics have the
-                    expertise to get you back on the road safely and efficiently. 
-                    Come visit us at 67 Main St East Machias, ME 04630, 
-                    and experience the difference that sets us apart.
-                </p>
+            <Section scrollRef="testimonials" heading="Testimonials">
+                <div class="flex flex-col max-w-full lg:flex-row gap-10">
+                    <Review rating={5} author="John N.">
+                        Last year, my car broke
+                        down in the middle of town, I was towed to Prescott's Garage.
+                        They worked on my car that morning, and I was back out by lunch.
+                    </Review>
+                    <Review rating={5} author="Brittany F.">
+                        We were on our way home from visiting family in Machias
+                        and had to get a tire patched. Prescott's Garage was amazing.
+                        They fixed it on the spot in 10 minutes.
+                    </Review>
+                    <Review rating={5} author="Michael C.">
+                        100% Satisfaction at a fair price. No BS, No taking advantage, No "Jacked Up Prices" Great work , on time results. Best of the Best.
+                    </Review>
+                </div>
             </Section>
             <Section scrollRef="contact" heading="Contact Us" image="/img/wagons.jpg">
                 <p class="text-justify">
