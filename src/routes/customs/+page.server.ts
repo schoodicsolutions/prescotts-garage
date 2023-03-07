27880b5b3577556037d678f0ebd5623f2e3641c2
Ryan Prescott <rppgoat@gmail.com>
2023-03-07 16:14:03 -0500

    Fix customs

--- src/routes/customs/+page.server.ts
+++ src/routes/customs/+page.server.ts
@@ -1,11 +1,22 @@
 import fs from 'fs';
 
-export async function load() {
+import { PUBLIC_BASEURL } from '$env/static/public';
+
+export const prerender = true;
+
+export async function load({ url }: { url: URL }) {
     const dir = await fs.promises.readdir('./static/img/gallery');
     const images = dir.filter(
         (filename) => filename.slice(-5).toLowerCase() === '.webp'
     ).map(
         (filename) => '/img/gallery/' + filename
     );
-    return { images }
+    return {
+        images,
+        title: 'Customs',
+        suffix: 'East Machias, Maine',
+        description: 'Your Go-To Auto Body Shop for Top-Quality Repairs and Services. Trust Our Experienced Technicians for all Your Vehicle Needs. Contact Us Today!',
+        canonical: `${PUBLIC_BASEURL}${url.pathname}`
+    }
 }
+
